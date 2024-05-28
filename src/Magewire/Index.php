7b11a2cf0dee33bd3bd826a4bfcd376849e30670
Ryan Grippeling <R.Grippeling@hotmail.com>
2024-05-28 16:15:44 +0200

    Added index

---
+++ src/Magewire/Index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace WebGrip\ExampleModule\Magewire;
+
+use Magewirephp\Magewire\Component;
+
+class Index extends Component
+{
+    public int $count = 100;
+
+    public function redirectToCounter()
+    {
+        $this->redirect('/webgrip/counter');
+    }
+
+    public function redirectToProductFilter()
+    {
+        $this->redirect('/webgrip/productfilter');
+    }
+}
