cb9735fdb60ebb8d5895e47871d86b928e43759d
Ryan Grippeling <R.Grippeling@hotmail.com>
2024-05-28 13:50:14 +0200

    Register to src

--- registration.php
+++ registration.php
@@ -7,5 +7,5 @@ use Magento\Framework\Component\ComponentRegistrar;
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'WebGrip_ExampleModule',
-    __DIR__
+    __DIR__ . '/src'
 );
