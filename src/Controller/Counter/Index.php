136db07be9cc1c352e790d5a9a4f99a7fc2216cd
Ryan Grippeling <R.Grippeling@hotmail.com>
2024-05-28 15:33:13 +0200

    It failed

--- src/Controller/Counter/Index.php
+++ src/Controller/Counter/Index.php
@@ -8,14 +8,8 @@ use Magento\Framework\View\Result\PageFactory;
 
 class Index implements ActionInterface
 {
-    private Context $context;
-
-    public function __construct(
-        Context $context,
-        PageFactory $pageFactory
-    ) {
-        $this->pageFactory = $pageFactory;
-        $this->context = $context;
+    public function __construct(private PageFactory $pageFactory)
+    {
     }
 
     public function execute()
